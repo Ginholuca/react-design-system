fe6a2cbd2880d8b6b1551768584fe74a45fbaa33
Giovanni De Luca <ginholucacorrea@icloud.com>
2022-10-13 23:30:57 -0300

    Tailwind-Colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
+    colors: {
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7c7c8a',
+      'gray-200': '#c4c4cc',
+      'gray-100': '#e1e1e6',
+
+      'cyan-500': '#81d8f7',
+      'cyan-100': '#bae7f6',
+    },
     extend: {
       fontFamily: {
         sans: 'Inter, sans-serif',
